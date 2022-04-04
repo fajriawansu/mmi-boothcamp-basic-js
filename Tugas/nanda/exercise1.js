3271e18f214792cc82ed70316a1b312574582c30
Ananda Novi Prasetya <cut3.chorom@gmail.com>
2022-04-04 13:56:07 +0700

    exercise 1 yang terbaru tentang objek

--- Tugas/nanda/exercise1.js
+++ Tugas/nanda/exercise1.js
@@ -33,3 +33,59 @@ const falseStart1 = url1.endsWith("dashboard");
 const falseStart2 = url2.endsWith("dashboard");
 console.log(falseStart1);
 console.log(falseStart2);
+
+// datatype number
+
+const phoneNumber = 628123456789;
+const num1 = 12.456352;
+
+//1. Ubah phoneNumber jadi string
+const ubahString = phoneNumber.toString();
+//2. Ubah num1 jadi bilangan bulat (global methods dan number methods)
+const bilanganBulat = num1.toFixed();
+
+// datatype object
+
+const kucingku = {
+    "nama": "Oren",
+    "kaki": 4,
+    "ekor": 1,
+    "teman": ["Garong"]
+  };
+
+//1. ubah nama jadi "Si Oren"
+kucingku.nama  = "Si Oren";
+console.log(kucingku.nama);
+//2. buat object baru, berisi data "kucingku" dan 2 properties baru
+const copyKucingku = {...kucingku,
+    punya_anak:"2",
+    hobi: "Bermain"}
+console.log(copyKucingku);
+//3. buat object dengan nama variabel "biodata" (include nested object dengan properties name "alamant", include "this", include method)
+const biodata = {
+    nama_depan: "Nanda",
+    nama_belakang: "Prasetya",
+    phone_number: "1123232",
+    nama_lengkap : function(){
+        return this.nama_depan + this.nama_belakang;
+    },
+    alamat: {
+        kota: "Tanggerang",
+        provinsi: "Banten",
+        alamat_lengkap:function(){
+            return "Jalan Sawah Darat " + this.kota + " " + this.provinsi;
+        }
+    }
+}
+console.log(biodata);
+//4. buat variabel yang mereturn nama depan, nama belakang, alamat lengkap, alamat (kota saja), nama lengkap
+const namaDepan1 = biodata.nama_depan;
+const namaBelakang = biodata.nama_belakang;
+const alamatLengkap = biodata.alamat.alamat_lengkap();
+const kotaTinggal = biodata.alamat.kota;
+const namaLengkapOrg =  biodata.nama_lengkap();
+console.log(namaDepan1);
+console.log(namaBelakang);
+console.log(alamatLengkap);
+console.log(kotaTinggal);
+console.log(namaLengkapOrg);
