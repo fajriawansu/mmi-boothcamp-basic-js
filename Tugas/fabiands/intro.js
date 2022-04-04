ad0a1c8068227aa57ea75986068bdc61d4c33496
Fabian Dewantara Santonie <fabian.dewantara20@gmail.com>
2022-04-04 15:37:01 +0700

    Sort & Filter

--- Tugas/fabiands/intro.js
+++ Tugas/fabiands/intro.js
@@ -55,3 +55,126 @@ function checknama(theObject, theProperties){
 const fullNama = biodata.nama_lengkap();
 //console.log(checknama(biodata,"nama_depan"));
 console.log(fullNama);
+
+const member = [
+    {
+        id : 4,
+        nama_depan : "Fabian",
+        nama_belakang : "Santonie",
+        phone_number : "088888",
+        alamat : {
+            "kota" : "bekasi",
+            "provinsi" : "jawa barat"
+            }
+    },
+    {
+        id : 2,
+        nama_depan : "Budi",
+        nama_belakang : "Alfianto",
+        phone_number : "088888",
+        alamat : {
+            "kota" : "bekasi",
+            "provinsi" : "jawa timur"
+            }
+    },
+    {
+        id : 3,
+        nama_depan : "Wawan",
+        nama_belakang : "Tutu",
+        phone_number : "088888",
+        alamat : {
+            "kota" : "bekasi",
+            "provinsi" : "jawa barat"
+            }
+    }
+]
+//console.log(member);
+//console.log(member[0]);
+
+const copyOfMember = [
+    ...member,
+    {
+        id : 1,
+        nama_depan : "Sukijan",
+        nama_belakang : "Saya",
+        phone_number : "088888",
+        alamat : {
+            "kota" : "bekasi",
+            "provinsi" : "jawa barat"
+            }
+    }
+]
+//console.log(copyOfMember);
+
+const hewan = ["Ayam", "Kucing", "Rusa", "Kambing"]
+const stringHewan = hewan.toString();
+const hewanTemp = [...hewan];
+const joinHewan = hewanTemp.join("-");
+hewanTemp.pop();
+hewanTemp.shift();
+console.log(hewanTemp);
+
+const namaArray1 = ["Wawan", "Tutu", "Tritri"];
+const namaArray2 = ["Forfor", "fivfiv", "sixsix", "sevsev", "egeg"];
+const namaArraygabung = namaArray1.concat("Ninnin");
+const namaArraysplice = namaArray1.splice(0,1,"Ahmad");
+const namaArrayslice = namaArray1.slice(1,2);
+
+//console.log(namaArraygabung);
+//console.log(namaArrayslice);
+//console.log(namaArraysplice);
+//console.log(namaArray1);
+
+const sortArray = namaArray2.sort();
+//console.log(sortArray);
+
+//Sort By ID
+function sortMember(){
+    return copyOfMember.sort((a,b)=>{
+        let before = a.id;
+        let after = b.id;
+        return a.id-b.id;
+    });
+}
+
+function sortMember2(){
+    return copyOfMember.sort(mySort);
+}
+function mySort(a,b){
+    return a.id - b.id;
+}
+
+function sortMember3(){
+    return copyOfMember.sort(function(a,b){
+        return parseInt(a.id) - b.id
+    });
+}
+
+//Sort By Name
+function myStringSort(a,b){
+    let before = a.nama_depan;
+    let after = b.nama_depan;
+    if(before <= after) {return -1;};
+    if(after > before) {return 1;};
+}
+
+//console.log(copyOfMember.sort(myStringSort));
+
+function learnForEach(v){
+    console.log(v);
+    console.log("namanya adalah "+v.nama_depan);
+}
+
+const newMapping = copyOfMember.map((value, index) => {
+    return {
+        fullname: value.nama_depan + " " + value.nama_belakang
+    }
+})
+
+const newFilter = copyOfMember.filter((value, index) => {
+    return value.alamat.provinsi === "jawa barat"
+})
+
+console.log(newFilter);
+
+console.log(newMapping);
