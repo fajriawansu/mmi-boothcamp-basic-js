2c012cf33921cdde41c7516390af7bd1f46fa8b6
Fajriawan Sutansa <f.sutansa46@gmail.com>
2022-04-04 13:36:25 +0700

    tambah exercise 1

--- Tugas/fajriawan/exercise-1.js
+++ Tugas/fajriawan/exercise-1.js
@@ -15,3 +15,25 @@ const url2 = "/dashboard/main/log";
 
 //5. Gunakan string method yang mereturn nilai "true" apabila diterapkan untuk url1 dan url2 (pakai function 1 parameter)
 //6. Gunakan string method yang mereturn nilai "false" apabila diterapkan untuk url1 dan url2  (pakai function 1 parameter)
+
+// datatype number
+
+const phoneNumber = 628123456789;
+const num1 = 12.456352;
+
+//1. Ubah phoneNumber jadi string
+//2. Ubah num1 jadi bilangan bulat (global methods dan number methods)
+
+// datatype object
+
+const kucingku = {
+    "nama": "Oren",
+    "kaki": 4,
+    "ekor": 1,
+    "teman": ["Garong"]
+  };
+
+//1. ubah nama jadi "Si Oren"
+//2. buat object baru, berisi data "kucingku" dan 2 properties baru
+//3. buat object dengan nama variabel "biodata" (include nested object dengan properties name "alamant", include "this", include method)
+//4. buat variabel yang mereturn nama depan, nama belakang, alamat lengkap, alamat (kota saja), nama lengkap
