a595e8a8f77e5cdb3dbc30011af85e865b282169
dioharvandy <dioharvandy21@gmail.com>
2022-04-04 15:35:22 +0700

    Tambah intro.js

--- Tugas/dio/intro.js
+++ Tugas/dio/intro.js
@@ -86,19 +86,82 @@ console.log(anggota[0].nama+" "+anggota[0].jurusan+" "+anggota[0].alamat.alamat_
 console.log(anggota[0].hobi);
 console.log(anggota[0].hobi.toString());
 console.log(anggota[0].hobi.join(";"));
+
 anggota[0].hobi.push("Listen");
 console.log(anggota[0].hobi);
+
 anggota[0].hobi.pop();
 console.log(anggota[0].hobi);
+
 anggota[0].hobi.shift();
 console.log(anggota[0].hobi);
+
 anggota[0].hobi.unshift("Play")
 console.log(anggota[0].hobi);
+
 console.log(anggota[0].hobi.length);
 console.log(anggota.length);
+
 const arr1 = [1,2,3]
 const arr2 = ["a", "b", "c", "d"]
 console.log(arr1+" "+arr2);
 arr1.splice(1,1, 4,5,6,7); //splice("index start tambah data arr", "banyak data dihapus mulai dari start", ..."data-data yg ditambah")
 console.log(arr1);
 console.log(arr2.slice(1,3));
+
+console.log(arr1.sort()+" "+arr2.sort())
+console.log(arr1.reverse()+" "+arr2.reverse())
+
+console.log(arr1.sort(function(a,b){return a-b}))
+console.log(arr1.sort(function(a,b){return b-a}))
+
+console.log(Math.max(1,22,3,4,5))
+console.log(Math.max.apply(null, [1,22,3,4,5]))
+
+const belajarSort = [
+    {nama: "Dio", umur: 21},
+    {nama: "Harvandy", umur: 25},
+    {nama: "Har", umur: 30},
+    {nama: "Vandy", umur: 23}
+]
+
+console.log(belajarSort.sort(function(a,b){return a.umur - b.umur}))
+belajarSort.sort(function a(a,b){
+    let x = a.nama.toLocaleLowerCase();
+    let y = b.nama.toLocaleLowerCase();
+    if(x<y){return -1 };
+    if(x>y){return 1 };
+    return 0;
+})
+console.log(belajarSort)
+
+belajarSort.forEach(iterationFunction)
+function iterationFunction(value, index, array){
+    return console.log(`nama orang ke-${index+1} adalah ${value.nama}`)
+}
+
+belajarSort.forEach((value, index, array)=>{
+    return console.log(`nama orang ke-${index+1} adalah ${value.nama}`)
+})
+
+console.log(arr1.map(mapFunction))
+function mapFunction(value, index, array){
+    return value*2;
+}
+
+console.log(arr1.map((value)=> value*3))
+
+console.log(arr1.filter(filterFunction))
+console.log(arr1.every(filterFunction)) //jika setiap element
+console.log(arr1.some(filterFunction)) // jika salah satu saja
+function filterFunction(value){
+    return value > 5
+}
+
+console.log(arr1.reduce(reduceOrSumFunction))
+function reduceOrSumFunction(total, value, index, array){
+    return total+value
+}
+
+console.log(belajarSort.findIndex((v)=> v.nama =="Dio"))
+console.log(belajarSort.find((v)=> v.nama =="Har"))
