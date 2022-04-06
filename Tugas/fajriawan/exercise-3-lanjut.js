31bb5e8242d3172b450b3a4498daa6ccda324030
Fajriawan Sutansa <f.sutansa46@gmail.com>
2022-04-06 16:36:17 +0700

    tambah 1 soal data relation

--- Tugas/fajriawan/exercise-3-lanjut.js
+++ Tugas/fajriawan/exercise-3-lanjut.js
@@ -1,4 +1,6 @@
 // tambahan jam 12 siang
+const warna = require("./dummyData").warna;
+const resep = require("./dummyData").resep;
 
 //basic js
 //1. buat function, yang mengecek tipe data arguments-arguments didalamnya
@@ -17,6 +19,20 @@
 // jika myFunc("#FF8") akan memiliki otput array ["#FF8C00", "#FF8243", "#FF85CF", ...]
 // notes: gunakan dataDummy warna
 
+const findHexcode = (str) => {
+    if(str.length > 3){
+        let output = [];
+        [...warna].forEach((v) => {
+            if(v.hex.includes(str)) output.push(v.hex);
+        })
+        return console.log(output);
+    } else {
+        return console.log("Kependekan")
+    }
+}
+
+// findHexcode("#594");
+
 //4. buat function contoh myFunc(foodName), yang mereturn object dengan 2 properties (steps dan ingredients)
 // steps dan ingredients merupakan data array
 // jika myFunc("Crock Pot Roast") output {steps: ["Place beef roast in crock pot.", ...], ingredients: ["beef roast", "water", ...]}
@@ -30,6 +46,19 @@
 // jika myFunc(60), output ["Ayam Goreng", "Sate Padang", ...]
 // notes: gunakan dataDummy resep
 
+const foodBelowThisMinutes = (minutes) => {
+    let output = [];
+
+    [...resep].forEach((v) => {
+        if(v.timers.reduce((total, value) => total + value) <= minutes){
+            output.push(v.name);
+        }
+    })
+    return console.log(output)
+}
+
+foodBelowThisMinutes(10);
+
 //7. buat function contoh myFunc(minutes, totalIngredients), yang mereturn array resep makanan
 // yang selesai <= minutes dan total ingredients <= totalIngredients
 // jika myFunc(60, 5), output ["Ayam Goreng", "Sate Padang", ...]
@@ -38,3 +67,24 @@
 //8. buat function contoh myFunc(theIngredients), yang mereturn array resep makanan yang membutuhkan bahan theIngredients
 // jika myFunc("Sugar"), output ["Ayam Goreng", "Sate Padang", ...]
 // notes: gunakan dataDummy resep
+
+const foodWithIngred = (theIngredients) => {
+
+    let output = [];
+
+    [...resep].forEach((v) => {
+        // siapin array buat di aksesnya
+        if(v.ingredients.some( value => value.name == theIngredients)){
+            output.push(v.name);
+        }
+    })
+
+    return console.log(output);
+}
+
+// foodWithIngred("raisins");
+
+// data relation
+//9. buat function contoh myFunc(), yang mereturn array mahasiswa yang 
+// menambah 2 properties baru yaitu "data_matkul" dan "data_dosbing"
+// tiap-tiap elementnya berisi object berdasarkan properties matkul_id dan dosbing_id
