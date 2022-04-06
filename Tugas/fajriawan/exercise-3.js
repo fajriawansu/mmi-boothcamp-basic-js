e5b4b8b39a3f8e8c4c6da9449a87be11dc764059
Fajriawan Sutansa <f.sutansa46@gmail.com>
2022-04-06 10:41:53 +0700

    tambah exercise 3

--- Tugas/fajriawan/exercise-3.js
+++ Tugas/fajriawan/exercise-3.js
@@ -10,4 +10,25 @@ const warna = require("./dummyData").warna;
 // jika myFunc("grey") akan memiliki output array ["Battleship Grey", "Cadet Grey", ...]
 //notes: gunakan dataDummy warna
 
+// basic js loop
+// 1. buat function, berisi loop sebanyak 21 kali (dari 0 sampai 20)
+// memiliki output berupa object dengan 2 properties, penjumlahan semua bilangan sesama genap dan sesama ganjil
+// contoh output: { "sum_all_genap" : 22, "sum_all_ganjil": 23 }
+
+// 2. buat function, berisi loop sebanyak 21 kali (dari 0 sampai 20)
+// memiliki output berupa object dengan 3 properties,
+// tiap-tiap properties merupakan array yang berisi bilangan prima, bilangan genap, bilangan ganjil
+// contoh output: { "genap" : [0,2,4,6, ...], "ganjil": [1,3,5,7], "prima": [2,3,5,7, ...] }
+
+// 3. buat function, berisi loop sebanyak 21 kali (dari 0 sampai 20)
+// jika number adalah kelipatan 2, dia akan menjadi element array berupa string "lipat2"
+// jika number adalah kelipatan 3, dia akan menjadi element array berupa string "lipat3"
+// jika number adalah kelipatan 2 dan 3, dian akan menjadi element array berupa string "lipat2dan3"
+// jika bukan merupakan kelipatan 2 ataupun 3, dia akan menjadi element array berupa angka itu sendiri
+// output: [0, 1,"lipat2", "lipat3", "lipat2", 5, "lipat2dan3", 7, ... ]
+
+//4. buat function, berisi loop dari 1 sampai 99
+// memiliki output (return) hasil penjumlahan bilangan-bilangan yang habis dibagi 5 atau 7
+// notes: angka-angka yang ditotal berarti: 5,7,10,14,15,20,21,25,28,dst
+
 console.log(warna[0]);
