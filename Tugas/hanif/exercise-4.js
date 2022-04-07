63b7e93943378e0ec21fe4ce3bba26bc9cfb3698
Hanif Dwitama Putera <hanif.puteraa@gmail.com>
2022-04-07 13:45:30 +0700

    tambah exercise 4

--- Tugas/hanif/exercise-4.js
+++ Tugas/hanif/exercise-4.js
@@ -2,10 +2,13 @@ const mahasiswa = require("./../fajriawan/dummyData").mahasiswa;
 
 // function call
 const stringMahasiswa = {
+    nama : "",
+    jumDosbing : 0,
+    jumMatkul : 0,
   fullData: function () {
     //jumlah parameters bebas, boleh kosong
     // contoj: "Udin memiliki 4 dosbing dan sedang mengambil 3 matkul"
-    return this.nama + " memiliki " + this.jumDosbing + " dan sedang mengambil " + this.jumMatkul + " matkul"; // //jumlah "this" bebas, tidak boleh kosong
+    return this.nama + " memiliki " + this.jumDosbing.length + " dosbing " + "dan sedang mengambil " + this.jumMatkul.length + " matkul"; // //jumlah "this" bebas, tidak boleh kosong
   },
 };
 
@@ -13,15 +16,15 @@ const myObject = {
   // jumlah properties beserta valuenya bebas
   // ini dinamis
   nama : "",
-  jumDosbing : [].length,
-  jumMatkul : [].length
+  jumDosbing : [],
+  jumMatkul : 0
 };
 myFunc(91)
 
 
 const myOutput = stringMahasiswa.fullData.call(myObject); // boleh tambah arguments selain "myObject", boleh tidak
 // contoh akan mereturn string "Udin memiliki 4 dosbing dan sedang mengambil 3 matkul"
-console.log(stringMahasiswa.fullData())
+console.log(myOutput)
 // soalnya ini:
 // buat function contoh myFunc(mahasiswaId); yang mereturn string
 
@@ -30,14 +33,13 @@ function myFunc(mhsId) {
     const myArray = [...mahasiswa]
 
     myArray.forEach((val, index)=> {
-        if (val.name == mhsId) {
-            nama = val.name
-            jumDosbing = val.dosbing_id
-            jumMatkul = val.matkul_id
+        if (val.id == mhsId) {
+            myObject.nama = val.name
+            myObject.jumDosbing = val.dosbing_id
+            myObject.jumMatkul = val.matkul_id
         }
     })
-    return myObject
+    return 
 }
 
 // dalam pengerjaannya wajib melibatkan variabel myOutput, dan stringMahasiswa
-console.log(myFunc(91))
