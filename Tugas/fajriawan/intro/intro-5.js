5f068035c6206c944f8a3f1668e485d0bfa098db
Fajriawan Sutansa <f.sutansa46@gmail.com>
2022-04-08 10:53:18 +0700

    materi jumat + exercise 4 jawaban bbrp

---
+++ Tugas/fajriawan/intro/intro-5.js
@@ -0,0 +1,17 @@
+// callback
+
+const myCallback = (string) => {
+    return console.log(string)
+}
+
+const myFunction = (cbFunction) => {
+    let data = "AAA";
+    setTimeout(() => {
+        data = "Success!"
+        cbFunction(data)
+    }, 1000)
+
+    // return console.log(data)
+}
+
+myFunction(myCallback)
