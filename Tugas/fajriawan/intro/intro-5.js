d0a356c1b61f37723faa5e44cf248ef2331cf2b1
Fajriawan Sutansa <f.sutansa46@gmail.com>
2022-04-08 11:22:38 +0700

    promise

--- Tugas/fajriawan/intro/intro-5.js
+++ Tugas/fajriawan/intro/intro-5.js
@@ -14,4 +14,42 @@ const myFunction = (cbFunction) => {
     // return console.log(data)
 }
 
-myFunction(myCallback)
+// myFunction(myCallback)
+
+const myPromiseCb = (str) => {
+    console.log(str)
+}
+
+const myCbSuccess = () => {
+    console.log('SUKSES!')
+}
+
+const myCbError = () => {
+    console.log('ERROR!')
+}
+
+// js promise
+function testPromise(passedInNum, cbSukses, cbError){
+    let myPromise = new Promise((resolve, reject) => {
+        // producing code
+        let isEven = passedInNum;
+    
+        // callback
+        if(isEven % 2 == 0){
+            resolve();
+        } else {
+          reject();
+        }
+    });
+    
+    myPromise.then(
+        function(){
+            cbSukses('sukses')
+        },
+        function(){
+            cbError('error')
+        }
+    )
+}
+
+testPromise(3, myCbSuccess, myCbError)
