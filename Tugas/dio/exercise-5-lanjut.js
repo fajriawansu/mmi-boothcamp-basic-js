3165e814bd0c456e9a0ba9b7d4bb12b7fc3f8c8a
dioharvandy <dioharvandy21@gmail.com>
2022-04-08 21:48:37 +0700

    no message

--- Tugas/dio/exercise-5-lanjut.js
+++ Tugas/dio/exercise-5-lanjut.js
@@ -14,7 +14,7 @@ const requestMovies = async (cbResp) => {
 }
 
 const cbReqMovies = (data) => {
-    let output = [...data].map(v => v.title);
+    const output = [...data].map(v => v.title);
     console.log(output)
 }
 
@@ -30,7 +30,7 @@ const requestItem = async (cbResp)=>{
 // mengirim data ke callback
 // callback harus mereturn output berupa array, nama-nama item yg dibeli saja
 const cbReqItem = (data)=>{
-    let output = [...data].map((v)=> v.name)
+    const output = [...data].map((v)=> v.name)
     console.log(output)
 }
 //requestItem(cbReqItem)
@@ -39,7 +39,7 @@ const cbReqItem = (data)=>{
 // mengirim data ke callback
 // callback harus mereturn output berupa number, yaitu subtotal harga pembelian (price*qty)
 const cbReqSubTot = (data)=>{
-    let output = [...data].map((v)=> v.price*v.qty).reduce((sum, v)=> sum+v)
+    const output = [...data].map((v)=> v.price*v.qty).reduce((sum, v)=> sum+v)
     console.log(output)
 }
 //requestItem(cbReqSubTot)
@@ -51,7 +51,7 @@ const cbReqSubTot = (data)=>{
 // contoh: [{nama_item: "Jaket", penjual: "PT AAA"}, {nama_item: "Baju", penjual: "PT BBB"}, ...]
 
 const cbReqItemPenj = (data)=>{
-    let output = [...data].map((v)=>{
+    const output = [...data].map((v)=>{
         penjual = v.type == "BARANG" ? v.barang.seller : v.type == "JASA" ? v.jasa.seller : false
         return {"nama_item": v.name, "penjual": penjual}})
     console.log(output)
@@ -65,7 +65,7 @@ const cbReqItemPenj = (data)=>{
 // contoh: [{nama_item: "Jaket", total_item: "4 Piece"}, {nama_item: "Minyak", total_item: "1 Liter"}, ...]
 
 const cbReqTotItem = (data)=>{
-    let output = [...data].map((v)=> {
+    const output = [...data].map((v)=> {
         unitName = v.type == "BARANG" ? v.barang.unit_name : v.type == "JASA" ? v.jasa.unit_name : false
         return {"nama_item":v.name, "total_item": v.qty+" "+unitName}
     })
